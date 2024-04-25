d9efeea0ab42ddfb47b330052f74e27c22f80402
Behrad Zarei <bdotz@outlook.com>
2024-04-25 20:04:07 +0330

    Add project files.

---
+++ PostmanCloneUI/Form1.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace PostmanCloneUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(11F, 28F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1100, 630);
+            Font = new Font("Segoe UI", 12F);
+            Margin = new Padding(4, 4, 4, 4);
+            Name = "Form1";
+            Text = "Dashboard";
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
---
+++ PostmanCloneUI/Form1.cs
@@ -0,0 +1,10 @@
+namespace PostmanCloneUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
