3b78b5987c5c8f1fd1bd3ad52295240cd88da2c1
Behrad Zarei <bdotz@outlook.com>
2024-04-27 22:03:23 +0330

    Design dashboard form and adding controls

--- PostmanCloneUI/Dashboard.Designer.cs
+++ PostmanCloneUI/Dashboard.Designer.cs
@@ -28,20 +28,128 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Dashboard));
+            apiLabel = new Label();
+            urlTextBox = new TextBox();
+            responseTextBox = new TextBox();
+            callApi = new Button();
+            formHeader = new Label();
+            statusStrip = new StatusStrip();
+            systemStatus = new ToolStripStatusLabel();
+            resultLabel = new Label();
+            statusStrip.SuspendLayout();
             SuspendLayout();
+            // 
+            // apiLabel
+            // 
+            apiLabel.AutoSize = true;
+            apiLabel.Font = new Font("Segoe UI", 16F);
+            apiLabel.Location = new Point(45, 86);
+            apiLabel.Name = "apiLabel";
+            apiLabel.Size = new Size(62, 37);
+            apiLabel.TabIndex = 0;
+            apiLabel.Text = "API:";
+            // 
+            // urlTextBox
+            // 
+            urlTextBox.Location = new Point(143, 74);
+            urlTextBox.Name = "urlTextBox";
+            urlTextBox.Size = new Size(724, 47);
+            urlTextBox.TabIndex = 2;
+            // 
+            // responseTextBox
+            // 
+            responseTextBox.BackColor = Color.White;
+            responseTextBox.BorderStyle = BorderStyle.FixedSingle;
+            responseTextBox.Location = new Point(45, 186);
+            responseTextBox.Multiline = true;
+            responseTextBox.Name = "responseTextBox";
+            responseTextBox.ReadOnly = true;
+            responseTextBox.ScrollBars = ScrollBars.Both;
+            responseTextBox.Size = new Size(922, 384);
+            responseTextBox.TabIndex = 3;
+            // 
+            // callApi
+            // 
+            callApi.Location = new Point(873, 74);
+            callApi.Name = "callApi";
+            callApi.Size = new Size(94, 49);
+            callApi.TabIndex = 4;
+            callApi.Text = "Go";
+            callApi.UseVisualStyleBackColor = true;
+            callApi.Click += callApi_Click;
+            // 
+            // formHeader
+            // 
+            formHeader.AutoSize = true;
+            formHeader.Location = new Point(35, 19);
+            formHeader.Name = "formHeader";
+            formHeader.Size = new Size(218, 41);
+            formHeader.TabIndex = 5;
+            formHeader.Text = "Postman Clone";
+            // 
+            // statusStrip
+            // 
+            statusStrip.ImageScalingSize = new Size(20, 20);
+            statusStrip.BackColor = Color.White;
+            statusStrip.Items.AddRange(new ToolStripItem[] { systemStatus });
+            statusStrip.Location = new Point(0, 581);
+            statusStrip.Name = "statusStrip";
+            statusStrip.Size = new Size(987, 38);
+            statusStrip.TabIndex = 6;
+            statusStrip.Text = "statusStrip";
+            // 
+            // systemStatus
+            // 
+            systemStatus.Font = new Font("Segoe UI", 14F);
+            systemStatus.Name = "systemStatus";
+            systemStatus.Size = new Size(78, 32);
+            systemStatus.Text = "Ready";
+            // 
+            // resultLabel
+            // 
+            resultLabel.AutoSize = true;
+            resultLabel.Font = new Font("Segoe UI", 16F);
+            resultLabel.Location = new Point(35, 142);
+            resultLabel.Name = "resultLabel";
+            resultLabel.Size = new Size(99, 37);
+            resultLabel.TabIndex = 7;
+            resultLabel.Text = "Results";
             // 
             // Dashboard
             // 
             AutoScaleDimensions = new SizeF(17F, 41F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(897, 513);
+            BackColor = Color.White;
+            ClientSize = new Size(987, 619);
+            Controls.Add(resultLabel);
+            Controls.Add(statusStrip);
+            Controls.Add(formHeader);
+            Controls.Add(callApi);
+            Controls.Add(responseTextBox);
+            Controls.Add(urlTextBox);
+            Controls.Add(apiLabel);
             Font = new Font("Segoe UI", 18F);
-            Margin = new Padding(6, 6, 6, 6);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Margin = new Padding(6);
             Name = "Dashboard";
             Text = "Postman Clone by Behrad Zarei";
+            statusStrip.ResumeLayout(false);
+            statusStrip.PerformLayout();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label apiLabel;
+        private ComboBox requestTypeComboBox;
+        private TextBox urlTextBox;
+        private TextBox responseTextBox;
+        private Button callApi;
+        private Label formHeader;
+        private StatusStrip statusStrip;
+        private Label resultLabel;
+        private ToolStripStatusLabel systemStatus;
     }
 }
--- PostmanCloneUI/Dashboard.cs
+++ PostmanCloneUI/Dashboard.cs
@@ -1,10 +1,32 @@
-namespace PostmanCloneUI
+namespace PostmanCloneUI;
+
+public partial class Dashboard : Form
 {
-    public partial class Dashboard : Form
+    public Dashboard()
     {
-        public Dashboard()
+        InitializeComponent();
+    }
+
+    private async void callApi_Click(object sender, EventArgs e)
+    {
+        //Validate the api url
+
+
+        try
         {
-            InitializeComponent();
+            systemStatus.Text = "Calling API...";
+
+            //Sample code  - replave the actual API call
+            await Task.Delay(2000);
+
+            systemStatus.Text = "Ready";
+
+
+        }
+        catch (Exception ex)
+        {
+            responseTextBox.Text = $"Error: {ex.Message}";
+            systemStatus.Text = "Error";
         }
     }
 }
